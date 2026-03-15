45b802bccd1c63bcdf57fbc24229f7feac56fdc4
Rihan200707 <rihanshaik1010@gmail.com>
2026-03-16 00:42:57 +0530

    first commit

---
+++ src/main/java/com/klef/fsad/exam/SpringMvcWebApplication.java
@@ -0,0 +1,13 @@
+package com.klef.fsad.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcWebApplication.class, args);
+	}
+
+}
